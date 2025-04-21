c6233b46fca1a810f60e6f4a4cceba7acfe33562
fylora <dvirm.biton@gmail.com>
2025-04-21 10:58:51 +0300

    Added drawables and icon configs

--- app/src/main/java/com/dvir/runique/MainActivity.kt
+++ app/src/main/java/com/dvir/runique/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.dvir.core.presentation.designsystem.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,31 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             RuniqueTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        Text(stringResource(R.string.app_name))
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun GreetingPreview() {
-    RuniqueTheme {
-        Greeting("Android")
     }
 }
